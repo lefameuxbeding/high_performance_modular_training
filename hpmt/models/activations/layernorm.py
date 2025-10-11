4d50e1fb377c8e4777b95cf848e0f17044021871
Charles-Étienne Joseph <90647079+lefameuxbeding@users.noreply.github.com>
2025-10-10 22:18:27 -0400

    feat: implement LayerNorm forward pass

--- hpmt/models/activations/layernorm.py
+++ hpmt/models/activations/layernorm.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class LayerNorm(nn.Module):
     def __init__(
         self,
-        normalized_shape: int | list | torch.Size,
+        normalized_shape: int | list[int] | torch.Size,
         eps: float = 1e-5,
         elementwise_affine: bool = True,
         bias: bool = True,
@@ -13,16 +13,21 @@ class LayerNorm(nn.Module):
         dtype: torch.dtype | None = None,
     ):
         super().__init__()
-        self.normalized_shape = normalized_shape
+        tuple_normalized_shape: tuple[int, ...] = (
+            (normalized_shape,)
+            if isinstance(normalized_shape, int)
+            else tuple(normalized_shape)
+        )
+        self.dim = list(range(-len(tuple_normalized_shape), 0))
         self.eps = eps
 
         if elementwise_affine:
             self.weight = nn.Parameter(
-                torch.ones(self.normalized_shape, device=device, dtype=dtype)
+                torch.ones(tuple_normalized_shape, device=device, dtype=dtype)
             )
             if bias:
                 self.bias = nn.Parameter(
-                    torch.zeros(self.normalized_shape, device=device, dtype=dtype)
+                    torch.zeros(tuple_normalized_shape, device=device, dtype=dtype)
                 )
             else:
                 self.register_parameter("bias", None)
@@ -30,5 +35,15 @@ class LayerNorm(nn.Module):
             self.register_parameter("weight", None)
             self.register_parameter("bias", None)
 
-    def forward(self, x):
-        return x
+    def forward(self, x: torch.Tensor):
+        mean = x.mean(dim=self.dim, keepdim=True)
+        var = x.var(dim=self.dim, unbiased=False, keepdim=True)
+
+        x_norm = (x - mean) / torch.sqrt(var + self.eps)
+
+        if self.weight is not None:
+            x_norm = x_norm * self.weight
+        if self.bias is not None:
+            x_norm = x_norm + self.bias
+
+        return x_norm
